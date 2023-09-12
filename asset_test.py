fe1ee2be7faff720346483689a47190ae5c538f8
Ewan Cowan <123670663+Wan-79@users.noreply.github.com>
2023-09-12 17:16:29 +0100

    Update asset_test.py

--- asset_test.py
+++ asset_test.py
@@ -1,1 +1,17 @@
+import pandas as pd
+import pymongo
+from pymongo import MongoClient
+import csv
+from datetime import datetime
+from pymongo import MongoClient
+import streamlit as st
+
+
+client = "mongodb://localhost:27017/"
+# Opening the mongo client that is given
+client = MongoClient(client)
+db = client["com_vtx"]
+asset_collection = db["Asset"]
+unique_suppliers = df['supplier'].unique().tolist()
+supplier = st.selectbox("Which supplier do you want asset info for?", unique_suppliers)
 
